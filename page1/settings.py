e7c77964a7563bbe60d7ccbe316897525bb9e813
Motlaq <MMotlaq@BPC.CO.IR>
2025-09-11 13:17:46 +0330

    add static root

--- page1/settings.py
+++ page1/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 
 # static
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
